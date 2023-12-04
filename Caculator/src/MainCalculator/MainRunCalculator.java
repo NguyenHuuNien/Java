83eff3229b70a55f15f09d15e6c291d87ab16461
Nguyễn Hữu Niên <huunien734@gmail.com>
2023-12-04 11:52:12 +0700

    buoi hoc oop dau tien

---
+++ Caculator/src/MainCalculator/MainRunCalculator.java
@@ -0,0 +1,12 @@
+package MainCalculator;
+
+import ui_thietke_calculator.UI_Calculator;
+
+public class MainRunCalculator {
+
+	public static void main(String[] args) {
+		UI_Calculator ui = new UI_Calculator("Chuong trinh may tinh co ban");
+		ui.showWindow();
+	}
+
+}
