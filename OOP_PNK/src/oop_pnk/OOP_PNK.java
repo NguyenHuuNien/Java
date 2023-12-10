19e626a8a65982370d9b15a720ac115a9dadadc2
Nguyễn Hữu Niên <125124829+NguyenHuuNien@users.noreply.github.com>
2023-12-10 10:17:58 +0700

    add code learn oop in pnk

--- OOP_PNK/src/oop_pnk/OOP_PNK.java
+++ OOP_PNK/src/oop_pnk/OOP_PNK.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class OOP_PNK {
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
         int y = sc.nextInt();
         System.out.println("Tong: " + (x+y));
