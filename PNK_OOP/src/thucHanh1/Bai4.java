08db19302f41d4457a11b3ba9798b105c58d7e3e
Nguyễn Hữu Niên <125124829+NguyenHuuNien@users.noreply.github.com>
2023-12-14 23:46:14 +0700

    update fix bai4

--- PNK_OOP/src/thucHanh1/Bai4.java
+++ PNK_OOP/src/thucHanh1/Bai4.java
@@ -1,12 +1,8 @@
 package thucHanh1;
-
 import java.util.ArrayList;
 import java.util.Scanner;
-
 public class Bai4 {
-
-	private static int dem = 1;
-	
+	private static long dem = 1;
 	public static void dao(int[] al) {
 		int i = al.length - 1;
 		while(i>0 && al[i]==2 && al[i-1]==2) {
@@ -17,10 +13,13 @@ public class Bai4 {
 				al[i-1] = 2;
 				dem++;
 				i--;
-				dao(al);
+				int[] ar = new int[al.length];
+				for(int j=0;j<al.length;j++) {
+					ar[j] = al[j];
+				}
+				dao(ar);
 			}
 	}
-	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Nhap so luong phan tu:");
@@ -33,22 +32,18 @@ public class Bai4 {
 			System.out.println("1");
 			System.exit(0);
 		}
-		
 		while(arr.get(0)==1 && arr.get(1) ==1) {
 			arr.remove(0); arr.remove(0);
 			arr.add(2);
 			dem++;
-			
 			Integer[] integer = new Integer[arr.size()];
 			integer = arr.toArray(integer);
 			int[] intArr = new int[arr.size()];
 			for(int i=0;i<arr.size();i++) {
 				intArr[i] = integer[i];
 			}
-			
 			dao(intArr);
 		}
 		System.out.println(dem);
 	}
-
 }
