6f55067f88cfcf49b3fcaa8d1f08806555fadbdf
Nguyễn Hữu Niên <125124829+NguyenHuuNien@users.noreply.github.com>
2024-02-23 00:50:13 +0700

    sua giang vien

--- QuanLyTrungTamNgonNgu/src/ObjectClass/GiangVien.java
+++ QuanLyTrungTamNgonNgu/src/ObjectClass/GiangVien.java
@@ -5,12 +5,11 @@ import java.io.Serializable;
 public class GiangVien extends People implements Serializable{
     private String TrinhDo;
 
-    
     public GiangVien() {
         super("GV");
     }
 
-    public GiangVien(String Name, int Tuoi,String gioiTinh ,String SoDienThoai, String QueQuan, String TrinhDo) {
+    public GiangVien(String Name,int Tuoi,String gioiTinh ,String SoDienThoai,String QueQuan,String TrinhDo) {
         super("GV",Name, Tuoi, gioiTinh,SoDienThoai, QueQuan);
         this.TrinhDo = TrinhDo;
     }
